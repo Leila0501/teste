e990eaf0700035188fe2998957bae4af177c7a7f
Leila0501 <leila0501@gmail.com>
2024-11-13 11:30:21 -0300

    Update aula8_mod3.py

--- aula8_mod3.py
+++ aula8_mod3.py
@@ -9,5 +9,7 @@ st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
-st.slider('Selecione um número', 0,100)
+x = st.slider('Selecione um número', 0,100)
+st.write('x')
+
 st.number_input('Selecione um número', 0,100)
