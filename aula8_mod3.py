bc1396c1cbab366d6cd28dc3a7052d3a54b4bdc8
Leila0501 <leila0501@gmail.com>
2024-11-13 11:30:46 -0300

    Update aula8_mod3.py

--- aula8_mod3.py
+++ aula8_mod3.py
@@ -10,6 +10,6 @@ st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 x = st.slider('Selecione um número', 0,100)
-st.write('x')
+st.write(x)
 
 st.number_input('Selecione um número', 0,100)
