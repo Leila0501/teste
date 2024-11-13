c104d2bdd4478b032f625798736384a55e64ae21
Leila0501 <leila0501@gmail.com>
2024-11-13 16:58:39 -0300

    Update aula8excer6.py

--- aula8excer6.py
+++ aula8excer6.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
-df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+df = pd.read_csv('/content/BR_LQs_CD2022i.csv')
 x = st.slider('Selecione um número', 0,8441)
 df.head(x)
