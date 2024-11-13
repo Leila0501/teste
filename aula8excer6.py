6d2faf9810698c114ce2d3d3b949f2e9b6617ad6
Leila0501 <leila0501@gmail.com>
2024-11-13 17:00:48 -0300

    Update aula8excer6.py

--- aula8excer6.py
+++ aula8excer6.py
@@ -1,4 +1,11 @@
+import requests as rq
+url = 'https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv.'
+resposta = rq.get(url)
+!pip install streamlit
+
 import streamlit as st
+st.title("Localização das comunidades quilombolas (2022)")
+
 import pandas as pd
 df = pd.read_csv('/content/BR_LQs_CD2022i.csv')
 x = st.slider('Selecione um número', 0,8441)
