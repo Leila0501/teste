d639347c8edcc77eb92f4d53181936b2723e478b
Leila0501 <leila0501@gmail.com>
2024-11-14 09:51:29 -0300

    Update aula8excer6.py

--- aula8excer6.py
+++ aula8excer6.py
@@ -1,15 +1,15 @@
 import requests as rq
-url = 'https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv.'
+url = 'https://github.com/Leila0501/teste/blob/main/BR_LQs_CD2022.csv'
 resposta = rq.get(url)
 
 import streamlit as st
 st.title("Localização das comunidades quilombolas (2022)")
 
 import pandas as pd
-df = pd.read_csv('/content/BR_LQs_CD2022i.csv')
-df = pd.DataFrame(url1)
+df = pd.read_csv(url)
+df
 
-x = st.slider('Selecione um número', 0,8441)
+x = st.slider('Selecione um número', 0,100)
 df.head(x)
 
 
