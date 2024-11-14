0177714113955409f7ab2abbd25a583072a9240d
Leila0501 <leila0501@gmail.com>
2024-11-14 12:41:11 -0300

    Update aula9exerc2.py

--- aula9exerc2.py
+++ aula9exerc2.py
@@ -37,4 +37,7 @@ st.header('Os dez municípios com mais comunidades quilombolas')#apresenta o tí
 df = df['NM_MUNIC'].value_counts().sort_values(ascending=False)[:10]
 st.bar_chart(df)
 
+st.metric('# Municípios', len(df['NM_MUNIC'].unique()))
+st.metric('# Comunidades', len(df['NM_AGLOM'].unique()))
+
 
