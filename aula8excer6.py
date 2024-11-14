c178eb583573958eb66fbae8c5c021f03bfe4fd6
Leila0501 <leila0501@gmail.com>
2024-11-14 09:56:11 -0300

    Update aula8excer6.py

--- aula8excer6.py
+++ aula8excer6.py
@@ -1,7 +1,4 @@
-import requests as rq
-url = 'https://github.com/Leila0501/teste/blob/main/BR_LQs_CD2022.csv'
-resposta = rq.get(url)
-
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 import streamlit as st
 st.title("Localização das comunidades quilombolas (2022)")
 
