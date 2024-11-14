103f41f381539fd10f1e089b43a58f6321dd7fb1
Leila0501 <leila0501@gmail.com>
2024-11-14 10:30:47 -0300

    Create aula9exerc2.py

---
+++ aula9exerc2.py
@@ -0,0 +1,17 @@
+st.title('Localização das comunidades quilombolas (2022)')
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+df
+#retirar coluna Unnamed
+
+df.drop(columns=['Unnamed: 0'], inplace=True)
+
+#converter colunas lat e long para números
+list=['Lat_d', 'Long_d']
+df[list] = df[list].apply(pd.to_numeric, errors='coerce')
+
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox('Selecione a UF', estados)
+dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
+st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
